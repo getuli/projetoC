941db35d73c3f745eb287a813e18631b65eb5c30
Getúlio Machado-Programação <getcontamach@gmail.com>
2024-03-06 08:16:43 -0300

    processo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+public class Algo{
+    int speed;
+
+    void Atribuir(){
+       speed = 2;
+
+       if(speed <= 5 || speed >= 0){
+        Console.WriteLine("Hello Word");
+       }else{
+        Console.WriteLine("Hello World 2");
+       }
+    }
+
+    void loop(){
+        int[] intArray = new int[]{1,2,3,4};
+        List<int> intList = new List<int>(){1,2,3,4};
+
+        foreach( int numbers in intList) {
+
+        }
+
+
+        for(int i = 0; i<= intList.Count; i++){
+
+            int list =+ i;
+
+        }
+
+        for( int i = 0; i <= intArray.Length; i++){
+            int array =+ i;
+            int currencyarray = intArray[i];
+            Console.WriteLine(currencyarray);
+        }
+    }
+
+}
